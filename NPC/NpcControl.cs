defe1b51edc3eb6fdb53d0631ca1ecf9c6487861
Barış Günenç <power.of.baris@gmail.com>
2020-07-25 10:17:13 +0300

    npccontrollere başladım devam edeceğim

---
+++ NPC/NpcControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcControl : MonoBehaviour
+{
+
+    //---- bu alan Destination managerdan gelecek
+    public Vector3 myDest;
+    public GameObject myDestInteractable;
+    //----------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
