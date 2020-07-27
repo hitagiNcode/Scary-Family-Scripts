5bab295311b1283277d5029f3800a8b691853edb
Barış Günenç <power.of.baris@gmail.com>
2020-07-27 13:39:23 +0300

    copied thirdpersonscript and updated npccontrol

--- NPC/NpcControl.cs
+++ NPC/NpcControl.cs
@@ -2,24 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+    [RequireComponent(typeof(UnityEngine.AI.NavMesh))]
 public class NpcControl : MonoBehaviour
 {
+    public Transform target;
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+
+    public ThirdPersonCharacter1  character { get; private set; }
 
     //---- bu alan Destination managerdan gelecek
     public Vector3 myDest;
+    // Oyuncu bir obje koyup attıgı zaman npc gidip onu alabilsin diye
     public GameObject myDestInteractable;
     //----------------------------------
 
-    // Start is called before the first frame update
+   
     void Start()
     {
-      
-        
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter1>();
+
+        agent.updateRotation = false;
+        agent.updatePosition = true;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (target != null)
+            agent.SetDestination(target.position);
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+            character.Move(agent.desiredVelocity, false, false);
+        else
+            character.Move(Vector3.zero, false, false);
+
     }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
 }
