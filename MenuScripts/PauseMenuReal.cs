ba08521adc63f783d640240337f70fb227ced7e1
Barış Günenç <power.of.baris@gmail.com>
2020-08-13 17:35:12 +0300

    Now Has A function can set UI deactive

--- MenuScripts/PauseMenuReal.cs
+++ MenuScripts/PauseMenuReal.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenuReal : MonoBehaviour
 {
     // Use this to send audio manager command to play pause menu music
     public static bool GameIsPaused = false;
 
-    //You need to drag and drop pausemenuUI in inspector to this boi
     public GameObject pauseMenuUI;
-
-    //You need to drag and drop optionsmenuUi in inspector
     public GameObject optionsMenuUI;
+
+    public GameObject PlayerUI;
+    public Image[] imagesUI;
+
+
 
     private void Start()
     {
         pauseMenuUI.SetActive(false);
+        //StartCoroutine(SetDeactiveTest());
+        //StartCoroutine(SetActiveTest());
     }
 
     void Update()
@@ -46,4 +51,36 @@ public class PauseMenuReal : MonoBehaviour
         optionsMenuUI.SetActive(true);
     }
 
+
+    public void SetPlayerUIDeactive()
+    {
+        PlayerUI.SetActive(false);
+        for (int i = 0; i < imagesUI.Length; i++)
+        {
+            imagesUI[i].enabled = false;
+        }
+    }
+
+    public void SetPlayerUIActive()
+    {
+        PlayerUI.SetActive(true);
+        for (int i = 0; i < imagesUI.Length; i++)
+        {
+            imagesUI[i].enabled = true;
+        }
+    }
+
+
+    IEnumerator SetDeactiveTest()
+    {
+        yield return new WaitForSeconds(5f);
+        SetPlayerUIDeactive();
+    }
+
+
+    IEnumerator SetActiveTest()
+    {
+        yield return new WaitForSeconds(15f);
+        SetPlayerUIActive();
+    }
 }
