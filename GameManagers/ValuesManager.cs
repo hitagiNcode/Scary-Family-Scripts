ef371aa23a0ecaca202fa99f81c081190bcd1e5c
Barış Günenç <power.of.baris@gmail.com>
2020-08-31 17:01:30 +0300

    StartingOfGameValues

---
+++ GameManagers/ValuesManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValuesManager : MonoBehaviour
+{
+    public static ValuesManager Instance {get; private set;}
+
+    public Text goldText;
+    public Text diamondText;
+    public Text livesText;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetValue("Gold", goldText);
+        GetValue("Diamond", diamondText);
+        GetValue("Lives", livesText);
+        
+    }
+
+
+
+    private void GetValue(string stringValue, Text inputText)
+    {
+        if (PlayerPrefs.HasKey(stringValue))
+        {
+            inputText.text = PlayerPrefs.GetInt(stringValue).ToString();
+        }
+        else
+        {
+            inputText.text = "0";
+        }
+    }
+
+    public void AddValue(string stringValue, Text inputText, int addAmount) 
+    {
+        int currentValue = PlayerPrefs.GetInt(stringValue, 0);
+
+        PlayerPrefs.SetInt(stringValue, currentValue + addAmount);
+
+        inputText.text = PlayerPrefs.GetInt(stringValue).ToString();
+    }
+
+
+    
+}
