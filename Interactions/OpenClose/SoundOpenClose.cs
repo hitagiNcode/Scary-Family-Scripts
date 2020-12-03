e1ef87b1de8b23cdb4d229b7d194cee9b2e7cd59
Barış Günenç <power.of.baris@gmail.com>
2020-12-03 21:40:59 +0300

    new script to openclsoe objects with 2 sound

---
+++ Interactions/OpenClose/SoundOpenClose.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOpenClose : InteractableObj
+{
+
+    private Animator _anim;
+    private bool isOpen;
+    private AudioSource m_source;
+
+    public AudioClip firstClip;
+    public AudioClip secondClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        m_source = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            _anim.SetBool("Open", true);
+            m_source.PlayOneShot(firstClip, 0.3f);
+        }
+        else
+        {
+            _anim.SetBool("Open", false);
+            m_source.PlayOneShot(secondClip, 0.3f);
+        }
+    }
+
+}
