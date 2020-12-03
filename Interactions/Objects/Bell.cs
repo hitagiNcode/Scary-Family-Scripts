64aa0a20c465ac5f352d477153998ccebdb64c71
Barış Günenç <power.of.baris@gmail.com>
2020-12-03 21:33:47 +0300

    bell added

---
+++ Interactions/Objects/Bell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bell : InteractableObj
+{
+    public NpcControl gelenNpc;
+    private AudioSource m_source;
+    public AudioClip ringClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_source = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+
+        m_source.PlayOneShot(ringClip, 0.3f);
+        gelenNpc.GoDoorBell(transform);
+    }
+
+}
